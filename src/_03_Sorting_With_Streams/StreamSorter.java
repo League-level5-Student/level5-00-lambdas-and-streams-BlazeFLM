89e75ebe1f0727d83066117f8e16c3b9fe05ac52
blazi <blazi@DESKTOP-9JBQAR7>
2024-11-05 18:34:03 -0800

    6:33 PM 11/5/2024

--- src/_03_Sorting_With_Streams/StreamSorter.java
+++ src/_03_Sorting_With_Streams/StreamSorter.java
@@ -21,11 +21,12 @@ import org.junit.jupiter.api.Test;
 public class StreamSorter {
 
     int[] sortArray(int[] arr) {
-        return null;
+    	arr = Arrays.stream(arr).sorted().toArray();
+        return arr;
     }
 
     List<String> sortList(List<String> list) {
-        return null;
+    	return list.stream().sorted().collect(Collectors.toList());
     }
     
     // =================== DO NOT MODIFY THE CODE BELOW ======================
